02e2169e3aa9e99f3062903ddc9139b6bd3836ae
Jacob Groundwater <groundwater@gmail.com>
2012-06-11 17:16:10 +0800

    Make New Controller Action to Authenticate Login Forms
    This is just the boiler plate to create a new Action. Right now the action just passes the request through to the next action.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,6 +5,20 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 
+trait Authenticate[A] extends Action[A]
+object Authenticate {
+  def apply[A](bodyParser: BodyParser[A])(block: Request[A] => Result) = new Authenticate[A] {
+    def parser = bodyParser
+    def apply( req: Request[A] ) = {
+      block(req)
+    }
+  }
+  def apply (block: Request[AnyContent] => Result): Action[AnyContent] = {
+    Authenticate(BodyParsers.parse.anyContent)(block)
+  }
+}
+ 
+
 object Application extends Controller {
   
   def index = Action {
@@ -20,12 +34,12 @@ object Application extends Controller {
       "username" -> text,
       "password" -> text))
       
-  def authenticate = Action{ implicit request =>
+  def authenticate = Authenticate{ Action{ implicit request =>
     loginForm.bindFromRequest.fold (
       hasErrors => BadRequest("Bad Request"),
       success   => Redirect(routes.Application.home(success._1))
     )
-  }
+  }}
   
   def home(id: String) = Action { implicit request =>
     Ok(views.html.home(id))
